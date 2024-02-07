8cf42867fe7c0f58cdb660c684c28221f38ecdaa
basit murad <basitmurad7@gmail.com>
2024-02-07 17:13:54 +0500

    custom shape

--- lib/features/shop/screens/home/HomeScreen.dart
+++ lib/features/shop/screens/home/HomeScreen.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import '../../../../common/widgets/custum_shape/container/MyAppPrimaryHeader.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return  Scaffold(
       backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            MyAppPrimaryHeaderContainer(
+
+              child:  Container(),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
+
+
