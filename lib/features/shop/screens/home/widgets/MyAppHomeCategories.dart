6fd1ec4106a91c6ab2bcd207430b1ffab557283e
basit murad <basitmurad7@gmail.com>
2024-02-08 23:54:04 +0500

    search view done 1

--- lib/features/shop/screens/home/widgets/MyAppHomeCategories.dart
+++ lib/features/shop/screens/home/widgets/MyAppHomeCategories.dart
@@ -12,14 +12,27 @@ class MyAppHomeCategories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 90,
+      height: 80,
       child: ListView.builder(
         itemCount: 6,
         shrinkWrap: true,
         scrollDirection: Axis.horizontal,
         itemBuilder: (_, index){
 
-          return   MyAppVerticalImageText(image:  MyAppImages.clothIcon, title: 'cloths' , ontap: (){}, textColor: Colors.white,);
+          List<Map<String, dynamic>> categoryData = [
+            {"image": MyAppImages.sportIcon, "title": "Sports"},
+            {"image": MyAppImages.clothIcon, "title": "Cloths"},
+            {"image": MyAppImages.shoeIcon, "title": "Shoes"},
+            {"image": MyAppImages.cosmeticsIcon, "title": "Cosmetics"},
+            {"image": MyAppImages.animalIcon, "title": "Animals"},
+            {"image": MyAppImages.toyIcon, "title": "Toys"},
+            // Add more categories as needed
+          ];
+
+          // Get the image and title data for the current index
+          String image = categoryData[index]["image"];
+          String string = categoryData[index]["title"];
+          return   MyAppVerticalImageText(image:  image, title: string , ontap: (){}, textColor: Colors.white,);
 
         },
       ),
